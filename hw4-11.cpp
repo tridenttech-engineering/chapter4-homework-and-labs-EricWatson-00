0e519e659305b20a86abddb7996e76af3583f4da
EricWatson-00 <ericjwat@gmail.com>
2024-07-12 20:58:59 -0400

    Update hw4-11.cpp

--- hw4-11.cpp
+++ hw4-11.cpp
@@ -1,11 +1,24 @@
 // Introductory11.cpp - displays ending balance
-// Created/revised by <your name> on <current date>
+// Created/revised by <Eric Watson> on <7/12/2024>
 
 #include <iostream>
 using namespace std;
 
 int main() {
-  
+  double salePrice=0.0;
+  double saleTaxRate=0.0;
+  double salesTax=0.0;
+  double cost=0.0;
+
+  cout<< "Enter the sale price: ";
+  cin>> salePrice
+  cout<< "Enter the sales tax rate: ";
+  cin>> saleTaxRate;
+
+  salesTax=salePrice*saleTaxRate;
+  cost=salePrice+salesTax
+
+  cout<< "cost $" << cost <<endl;
 
   return 0;
 } // end of main function
